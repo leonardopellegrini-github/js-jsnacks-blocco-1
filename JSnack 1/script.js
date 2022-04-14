46380392e5b924814198fcdd43b464af24d1e0bb
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-04-14 16:04:13 +0200

    es 1 fatto

--- JSnack 1/script.js
+++ JSnack 1/script.js
@@ -3,3 +3,11 @@
  L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
 */
 
+let primoNumero = prompt ("Inserisci il primo numero")
+let secondoNumero = prompt ("Inserisci il secondo numero")
+
+if (primoNumero > secondoNumero){
+  document.getElementById("Numeromaggiore").innerHTML = "Il numero maggiore è" + " " + primoNumero;
+} else {
+  document.getElementById("Numeromaggiore").innerHTML = "Il numero maggiore è" + " " + secondoNumero;
+} 

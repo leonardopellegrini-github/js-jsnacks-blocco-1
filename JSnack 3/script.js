109c97d1b3b11c23e0e5bfc88991818de1ae29df
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-04-19 11:38:46 +0200

    completamento js 3

--- JSnack 3/script.js
+++ JSnack 3/script.js
@@ -4,3 +4,15 @@
  Il programma stampa la somma di tutti i numeri inseriti.
  Esegui questo programma in due versioni, con il for e con il while (facoltativo)
 */
+
+let totale = 0;
+
+for (let i = 0; i < 5; i++){
+
+  let numero = parseInt(prompt("Inserisci un numero"));
+  console.log(i);
+  totale = totale + numero;
+  document.getElementById("totale").innerHTML = totale;
+  console.log(totale);
+}
+
--- JSnack 4/script.js
+++ JSnack 4/script.js
@@ -3,3 +3,6 @@
  In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
  chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 */
+
+database = ['gianni'; 'pinotto'; 'angelo'; 'fabrizio'; 'nicolo'; 'margherita';]
+
